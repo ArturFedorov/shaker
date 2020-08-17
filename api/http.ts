3db144dd4a7c23ae3292d49c7812693763edb17d
artur_fjodorov <artur.fodorov@gmail.com>
2020-08-18 01:54:27 +0300

    http service

---
+++ api/http.ts
@@ -0,0 +1,32 @@
+import { Logger } from '../server-configs/Logger.ts';
+import ApiError from './ApiError.ts';
+import {HttpMethods} from './HttpMethods.ts';
+
+export class http {
+  static GET<T>(url: string, options: RequestInit) {
+    options.method = HttpMethods.GET;
+    return this.makeRequest(url, options);
+  }
+
+  static POST<T>(url: string, options: RequestInit) {
+    options.method = HttpMethods.POST;
+    return this.makeRequest(url, options);
+  }
+
+
+  private static makeRequest<T>(url: string, options: RequestInit) {
+    return fetch(url, options)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network error has occured');
+        }
+
+        return response.json() as Promise<T>
+      })
+      .catch((error) => {
+        const message = `Error while requesting to ${url}. Error: ${error.message}`;
+        Logger.appLogger.error(message)
+        return  Promise.reject(new ApiError(message, error.code));
+      })
+  }
+}
